3336232c7b30a1c47b0008e91b9c333a90d703eb
David Vantaelen <vant.david@outlook.com>
2024-01-31 16:43:41 +0100

    Preact Integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import preact from '@astrojs/preact';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()]
+    output: 'server',
+    adapter: vercel(),
+    integrations: [
+        preact(), 
+        tailwind()
+    ]
 });
