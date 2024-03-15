160016f24038ca3d4193e7fc5be15f0067781771
Stephane Stadler <stephanestadler@mbp-de-stephane.home>
2024-03-15 19:17:58 +0100

    change output setting to debug build image on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
+    output: 'hybrid',
     adapter: vercel(),
     integrations: [
         react(), 
